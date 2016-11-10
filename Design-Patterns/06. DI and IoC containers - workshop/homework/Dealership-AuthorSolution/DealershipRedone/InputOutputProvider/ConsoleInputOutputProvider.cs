2b61a3470d3fe64d25b16838fea6806dfb054c82
shopov91 <shopov91@gmail.com>
2016-11-10 13:39:55 +0200

    IInputOutputProvider implemented

---
+++ Design-Patterns/06. DI and IoC containers - workshop/homework/Dealership-AuthorSolution/DealershipRedone/InputOutputProvider/ConsoleInputOutputProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealershipRedone.InputOutputProvider
+{
+    public class ConsoleInputOutputProvider : IInputOutputProvider
+    {
+        public string ReadInput()
+        {
+            return Console.ReadLine();
+        }
+
+        public void WriteOutput(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
