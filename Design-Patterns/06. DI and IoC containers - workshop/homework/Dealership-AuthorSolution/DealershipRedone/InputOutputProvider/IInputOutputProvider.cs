ce3579c9d1cc2dfacb6b7a3f4e4fc1bc3162bd27
shopov91 <shopov91@gmail.com>
2016-11-10 13:34:12 +0200

    Added IInputOutputProvider interface

---
+++ Design-Patterns/06. DI and IoC containers - workshop/homework/Dealership-AuthorSolution/DealershipRedone/InputOutputProvider/IInputOutputProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace DealershipRedone.InputOutputProvider
+{
+    public interface IInputOutputProvider
+    {
+        string ReadInput();
+
+        void WriteOutput(string value);
+    }
+}
