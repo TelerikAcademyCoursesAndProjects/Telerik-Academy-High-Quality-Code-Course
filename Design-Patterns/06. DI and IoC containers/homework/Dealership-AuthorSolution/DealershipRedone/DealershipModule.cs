046bfcf2b46f67c71350445d9d7ea2a0da9c0d36
shopov91 <shopov91@gmail.com>
2016-11-10 20:37:08 +0200

    Added bindings

--- Design-Patterns/06. DI and IoC containers/homework/Dealership-AuthorSolution/DealershipRedone/DealershipModule.cs
+++ Design-Patterns/06. DI and IoC containers/homework/Dealership-AuthorSolution/DealershipRedone/DealershipModule.cs
@@ -1,4 +1,9 @@
-﻿using DealershipRedone.InputOutputProvider;
+﻿using Dealership.Contracts;
+using Dealership.Engine;
+using Dealership.Factories;
+using Dealership.Models;
+using DealershipRedone.Factories;
+using DealershipRedone.InputOutputProvider;
 using Ninject;
 using Ninject.Extensions.Conventions;
 using Ninject.Extensions.Factory;
@@ -8,7 +13,6 @@ using Ninject.Parameters;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 
 namespace DealershipRedone
@@ -24,8 +28,28 @@ namespace DealershipRedone
                 .BindDefaultInterface();
             });
 
-          Bind<IInputOutputProvider>().To<ConsoleInputOutputProvider>().InSingletonScope();
-            
+            Bind<IVehicle>().To<Vehicle>();
+
+            Bind<ICar>().To<Car>();
+
+            Bind<ITruck>().To<Truck>();
+
+            Bind<IMotorcycle>().To<Motorcycle>();
+
+            Bind<IUser>().To<User>();
+
+            Bind<ICommand>().To<Command>();
+
+            Bind<IComment>().To<Comment>();
+
+            Bind<IInputOutputProvider>().To<ConsoleInputOutputProvider>().InSingletonScope();
+
+            Bind<IEngine>().To<DealershipEngine>().InSingletonScope();
+
+            Bind<IDealershipFactory>().ToFactory().InSingletonScope();
+
+            Bind<ICommandFactory>().ToFactory().InSingletonScope();
+
         }
     }
 }
