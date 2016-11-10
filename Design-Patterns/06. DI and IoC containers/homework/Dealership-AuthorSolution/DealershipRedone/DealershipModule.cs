3e0cb63cbe14d868665ae70fc492700fb71195e1
shopov91 <shopov91@gmail.com>
2016-11-10 13:56:44 +0200

    Added DealeshipModule and kernel bind

---
+++ Design-Patterns/06. DI and IoC containers/homework/Dealership-AuthorSolution/DealershipRedone/DealershipModule.cs
@@ -0,0 +1,27 @@
+﻿using Ninject;
+using Ninject.Extensions.Conventions;
+using Ninject.Extensions.Factory;
+using Ninject.Extensions.Interception.Infrastructure.Language;
+using Ninject.Modules;
+using Ninject.Parameters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace DealershipRedone
+{
+    public class DealershipModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Kernel.Bind(x =>
+            {
+                x.FromAssembliesInPath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+                .SelectAllClasses()
+                .BindDefaultInterface();
+            });
+        }
+    }
+}
